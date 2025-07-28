8e8b97e8402a181ee9d13b89883ab25fada96e60
Фоминых Денис Григорьевич <fominikh_d@goldapple.ru>
2025-07-28 19:41:23 +0300

    local

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f3af755ad6ed6ffdd96f0be891da5325",
+const RESOURCES = {"flutter_bootstrap.js": "3f2936116678ac8b46e56c1350668a64",
 "version.json": "b269a22a1dc852793434ca7d25e274c4",
 "index.html": "349c3457bcdd55e749743fc09c5a5e3e",
 "/": "349c3457bcdd55e749743fc09c5a5e3e",
